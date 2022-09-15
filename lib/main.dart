8326d0d040dc39572440d353f7f33c77ca0a1f61
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2022-09-15 18:52:04 +0100

    Section 2 : 32

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+//void main() {
+//  runApp(MyApp());
+//}
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    void answerQuestion() {
+      print('Answer chosen!');
+    }
+
+    var questions = [
+      'Whats your favorite color?',
+      'Whats your favorite animal'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The Question'),
+            ElevatedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text('Answer 2'),
+                onPressed: answerQuestion),
+            ElevatedButton(
+                child: Text('Answer 3'),
+                onPressed: answerQuestion),
+          ],
+        ),
+      ),
+    );
   }
 }
