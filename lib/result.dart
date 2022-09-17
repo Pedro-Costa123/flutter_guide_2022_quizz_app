4a2bc94eb3bcb7aec360a1fc6138a8d22c4003c7
Pedro-Costa123 <pedrocostaalves@live.com.pt>
2022-09-17 10:16:33 +0100

    Section 2 : 56 - END

--- lib/result.dart
+++ lib/result.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
   String get resultPhrase {
     String resultText;
     if (resultScore <= 8) {
       resultText = 'Good Score';
-    } else if(resultScore <=12) {
+    } else if (resultScore <= 12) {
       resultText = 'Cool Score';
-    } else if(resultScore <= 16) {
+    } else if (resultScore <= 16) {
       resultText = 'Meh Score';
     } else {
       resultText = 'Bad Score';
@@ -22,9 +23,19 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+          ),
+          TextButton(
+              onPressed: resetHandler,
+              child: Text('Restart Quiz!'),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.blue),
+              ))
+        ],
       ),
     );
   }
